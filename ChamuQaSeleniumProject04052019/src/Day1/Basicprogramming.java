090d63ee670b31f2093e48b5d6209db329eb180d
sadapa <sadapa@cam-nadapa-2.cochlear.com>
2019-04-11 09:08:15 -0600

    My second commit

---
+++ ChamuQaSeleniumProject04052019/src/Day1/Basicprogramming.java
@@ -0,0 +1,16 @@
+package Day1;
+
+public class Basicprogramming {
+	
+	public static void main (String [] args) {
+	
+	int num = 10;
+	int num2 = 20;
+	
+	int sum = num + num2;
+	
+	System.out.println("Total:" + sum);
+	
+}
+
+}

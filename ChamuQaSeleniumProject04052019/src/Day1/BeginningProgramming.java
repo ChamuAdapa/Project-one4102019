300ee7ed708b53df6a574da3c61af2f3a8409da3
sadapa <sadapa@cam-nadapa-2.cochlear.com>
2019-04-11 09:52:16 -0600

    my third commit test

--- ChamuQaSeleniumProject04052019/src/Day1/BeginningProgramming.java
+++ ChamuQaSeleniumProject04052019/src/Day1/BeginningProgramming.java
@@ -1,17 +1,20 @@
+
 package Day1;
 
-public class Basicprogramming {
+public class BeginningProgramming {
 	
 	public static void main (String [] args) {
 	
 	int num = 10;
 	int num2 = 20;
 	int num3 = 5;
+	int num4 =5;
 	
-	int sum = num + num2 + num3;
+	int sum = num + num2 + num3 + num4;
 	
 	System.out.println("Total:" + sum);
 	
 }
 
 }
+
